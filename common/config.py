b5e9f57a2373f75a017f7d2b070089110b87bd77
Lzxxxx <18708116679@163.com>
2024-03-15 17:03:36 +0800

    refactor

--- common/config.py
+++ common/config.py
@@ -10,6 +10,6 @@ class Config:
     OPENAI_MODEL = 'gpt-3.5-turbo'
     SQL_DDL_DML_FILE_PATH = '/Users/liuzhongxu/PycharmProjects/code_interpreter/database.sql'
     CODE_PATH = '/Users/liuzhongxu/Documents/Learning/demo 2/src/main'
-    os.environ['OPENAI_API_KEY'] = 'sk-hoZTk62z0b57ff585b04T3BlbKFJF9Ad5F81CC3d41f1BA4c'
-    os.environ['OPENAI_API_BASE'] = 'https://api.ohmygpt.com/v1'
+    os.environ['OPENAI_API_KEY'] = ''
+    os.environ['OPENAI_API_BASE'] = ''
 
